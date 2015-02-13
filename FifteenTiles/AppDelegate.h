b5da106af2f76d5ff0b63a749168fee5fe6530d2
Aaron Robinson <aaron@bpwebdesign.com>
2015-02-12 20:21:25 -0800

    Initial Commit

---
+++ FifteenTiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FifteenTiles
+//
+//  Created by Aaron Robinson on 2/12/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FifteenTiles/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FifteenTiles
+//
+//  Created by Aaron Robinson on 2/12/15.
+//  Copyright (c) 2015 SSU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
